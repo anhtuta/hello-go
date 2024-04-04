49d099c2f13be42c75653cd94b8534f34172ce06
Tu Ta Anh <tuta@deputy.com>
2024-04-04 13:38:27 +0700

    How to use array, map and for loop
    https://go.dev/doc/tutorial/greetings-multiple-people

--- hello/hello.go
+++ hello/hello.go
@@ -18,13 +18,16 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Gladys")
+	// A slice of names.
+	names := []string{"Gladys", "", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of messages to the console..
+	fmt.Println(messages)
 }
