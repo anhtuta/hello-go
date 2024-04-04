d76ed1382b43a7d262f281526c9dcfe799666d8e
Tu Ta Anh <tuta@deputy.com>
2024-04-04 11:34:59 +0700

    Init commit

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/greetings"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
