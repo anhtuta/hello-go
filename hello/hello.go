375db82ef9fdc3859f42c2a217b24819539805cc
Tu Ta Anh <tuta@deputy.com>
2024-04-04 12:18:38 +0700

    Handle error
    https://go.dev/doc/tutorial/handle-errors

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"hello-go/greetings"
 
@@ -9,9 +10,27 @@ import (
 )
 
 func main() {
+	// Set properties of the predefined Logger, including the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("[greetings]: ")
+	log.SetFlags(0)
+
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
+	// Request a greeting message.
+	message, err = greetings.Hello("")
+	// If an error was returned, print it to the console and exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message to the console.
 	fmt.Println(message)
 }
